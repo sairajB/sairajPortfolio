07db9c3d086266c592cd9d07d9d87b4ec517d604
sairajB <sairaj.sab@gmail.com>
2025-01-30 13:09:53 +0530

    modified:   src/sections/Hero/Hero.jsx

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -40,7 +40,7 @@ function Hero() {
           <br />
           Bodhale
         </h1>
-        <h2>Frontend Developer</h2>
+        <h2>Fullstack Developer</h2>
         <span>
           <a href="https://twitter.com/" target="_blank">
             <img src={twitterIcon} alt="Twitter icon" />
